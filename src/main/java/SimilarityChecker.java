00dabdcc21b845e5a13d480f0300e0b930b33e96
hyehye <hye1.jeong@samsung.com>
2025-06-04 17:11:56 +0900

    calcScoreByLength 함수 리팩토링 및 상수 추가

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,25 +1,28 @@
 public class SimilarityChecker {
 
+    private static final int MAX_SCORE = 60;
+
     public int getLengthScore(String A, String B) {
         int lengthA = A.length();
         int lengthB = B.length();
 
-        if (lengthA > lengthB) {
-            return calcScoreByLength(lengthA, lengthB);
-        } else if (lengthA < lengthB) {
-            return calcScoreByLength(lengthB, lengthA);
-        } else {
-            return 60;
+        if (lengthA == lengthB) {
+            return MAX_SCORE;
         }
+
+        return calcScoreByLength(lengthA, lengthB);
     }
 
-    private int calcScoreByLength(int bigNumberLength, int smallNumberLength) {
+    private int calcScoreByLength(int lengthA, int lengthB) {
+        int bigNumberLength = Math.max(lengthA, lengthB);
+        int smallNumberLength = Math.min(lengthA, lengthB);
+
         if (bigNumberLength >= smallNumberLength * 2) {
             return 0;
         }
 
         int gap = bigNumberLength - smallNumberLength;
-        double result = (1 - (double) gap / smallNumberLength) * 60;
+        double result = (1 - (double) gap / smallNumberLength) * MAX_SCORE;
         return (int) result;
     }
 
